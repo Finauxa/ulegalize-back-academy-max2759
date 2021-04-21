399da469e5b004644dba24a3f124f28bcf213498
max27 <max1>
2021-04-21 14:56:59 +0200

    Add & get comment

--- src/main/java/com/ulegalize/controller/CommentController.java
+++ src/main/java/com/ulegalize/controller/CommentController.java
@@ -1,13 +1,31 @@
 package com.ulegalize.controller;
 
+import com.ulegalize.model.Comment;
+import com.ulegalize.service.impl.CommentServiceImpl;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("v1/comments")
+@RequestMapping("v1")
 @Slf4j
 public class CommentController {
+
+    @Autowired
+    private CommentServiceImpl commentService;
+
+    @GetMapping(value = "/comments", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Comment> getComments(){
+        return commentService.getAllComments();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Comment addComment(@RequestBody Comment comment){
+        return commentService.createComment(comment);
+    }
 
 
 }
--- src/main/java/com/ulegalize/service/impl/CommentServiceImpl.java
+++ src/main/java/com/ulegalize/service/impl/CommentServiceImpl.java
@@ -1,8 +1,29 @@
 package com.ulegalize.service.impl;
 
+import com.ulegalize.model.Comment;
+import com.ulegalize.repository.CommentRepository;
 import com.ulegalize.service.ICommentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class CommentServiceImpl implements ICommentService {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    public List<Comment> getAllComments()
+    {
+        return commentRepository.findAll();
+    }
+
+    public Comment createComment(Comment comment){
+
+        LocalDateTime lt = LocalDateTime.now();
+        comment.setDate(lt);
+        return commentRepository.save(comment);
+    }
 }
